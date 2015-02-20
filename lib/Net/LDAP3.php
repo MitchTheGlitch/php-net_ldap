7e6148385886be88e891e192d7d1703960a9785e
Aleksander Machniak <machniak@kolabsys.com>
2015-02-20 10:45:24 +0100

    Make sure ldap filter is not empty when searching, use (objectclass=*) by default
    ldap_search() throws errors on empty filter as find out in
    http://trac.roundcube.net/ticket/1490269. As this was a configuration
    issue we can simply prevent that and provide real and working default.

--- lib/Net/LDAP3.php
+++ lib/Net/LDAP3.php
@@ -1552,12 +1552,16 @@ class Net_LDAP3
             return false;
         }
 
-        $this->_debug("C: Search base dn: [$base_dn] scope [$scope] with filter [$filter]");
-
         // make sure attributes list is not empty
         if (empty($attrs)) {
             $attrs = array('dn');
         }
+        // make sure filter is not empty
+        if (empty($filter)) {
+            $filter = '(objectclass=*)';
+        }
+
+        $this->_debug("C: Search base dn: [$base_dn] scope [$scope] with filter [$filter]");
 
         $function = self::scope_to_function($scope, $ns_function);
 
